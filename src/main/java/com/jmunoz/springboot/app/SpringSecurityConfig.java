2b79c96aff360ed00bf92ea9ee59fc356ff72b1b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-06 20:57:41 +0200

    Proteger rutas en nuestro API Rest. Parte 2

--- src/main/java/com/jmunoz/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/jmunoz/springboot/app/SpringSecurityConfig.java
@@ -36,35 +36,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // Quitamos el acceso a /api/clientes porque le vamos a dar seguridad.
         //
-        // Para ver sus datos, volver a ponerlo y, en Postman, hacer una petición GET a
-        // http://localhost:8080/api/clientes/listar
-        //
-        // Pero si lo quitamos, si vamos a Postman y ejecutamos ese GET, como ahora tiene seguridad, redirige a Login
-        // y veremos el código HTML de la vista Login
-        //
-        // Pera esa no es la idea, ya que queremos que nos muestre el código 401 Not Authorized o 403 Forbidden.
-        // Es por esto que hay incompatibilidad entre seguridad normal con sesiones con formulario Login y
-        // seguridad basada en token. No se ajusta bien a una o a otra.
-        // Lo idea es tener completamente separado nuestra aplicación para Rest de la parte Web.
-        // Pero se puede mezclar, lo que pasa es que no se recomienda.
-        http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale").permitAll()
+        // Quitamos el formulario Login y el Logout y el accessDeniedPage, ya que se va a manejar de forma
+        // automática con los códigos http.
+        // Si ahora en Postman ejecutamos: http://localhost:8080/api/clientes/listar
+        // Nos da el error 403, que es lo que queremos
+        // NOTA: Por ahora no aparece ningún json, solo se indica el error
+        http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar/**", "/locale").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                    .formLogin()
-                        .successHandler(successHandler)
-                        .loginPage("/login")
-                    .permitAll()
-                .and()
-                .logout().permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/error_403")
-                .and()
-                // 1. Deshabilitamos la protección csrf porque usaremos jwt y no el token de protección de csrf.
-                // csrf es más que nada para cuando se trabaja con formularios de Spring más que con Rest.
-                // Tenemos que quitar los input csrf que tengamos en los formularios de forma explícita.
                 .csrf().disable()
-                // 2. Configurar Spring Security, habilitando la configuración en el Session Manager como stateless
-                // por sobre el uso de sesiones.
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     }
