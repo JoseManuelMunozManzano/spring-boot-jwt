88e854e01195f076574f71d723e2e0889e3fe4f4
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-09 20:01:35 +0200

    Generando el JWT y añadiendo dependencias en el POM

--- src/main/java/com/jmunoz/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/jmunoz/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,19 +1,32 @@
 package com.jmunoz.springboot.app.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 // Por defecto, este filtro solo se va a ejecutar cuando nuestra ruta sea login del tipo POST.
 // Pero lo hemos cambiado para que nuestra ruta sea /api/login
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     private AuthenticationManager authenticationManager;
 
@@ -36,6 +49,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             logger.info("Password desde request parameter (form-data): " + password);
         }
 
+        // Dejar claro que este no es el JSON Web Token
+        // Este token se maneja de forma interna en nuestra aplicación con Spring Security y a partir de el
+        // objeto Authentication se puede obtener el username y todos los datos necesarios para crear el
+        // JSON Web Token
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
 
         // Se devuelve la autenticación
@@ -53,5 +70,50 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         // Forma 2: Seleccionar raw y JSON   (Todavía no está hecho)
         //   Mandar un JSON con el usuario y la contraseña
         return authenticationManager.authenticate(authToken);
+    }
+
+    // Aquí vemos el objeto Authentication authResult que es el mismo que tenemos arriba, con la diferencia
+    // de que aquí ya está autenticado (atributo authenticated en true) y con los datos del usuario, username,
+    // sus roles.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        // Empezamos a crear el JSON Web Token que tenemos que retornar al cliente. Por ahora sencillo.
+        // Falta agregar roles y la fecha de expiración
+        //
+        // Hay varias formas de obtener el nombre del usuario:
+        // authResult.getName()
+        // ((User) authResult.getPrincipal()).getUsername()
+        String username = ((User) authResult.getPrincipal()).getUsername();
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .signWith(SECRET_KEY)
+                .compact();
+
+        // Pasamos el token en la cabecera de la respuesta.
+        // Es obligatorio que el nombre del parámetro sea Authorization
+        // El valor del token, como standard, se inicia con el prefijo Bearer, seguido de espacio y el token
+        // Cuando el cliente envíe el token, también tendrá que poner el prefijo "Bearer "
+        response.addHeader("Authorization", "Bearer " + token);
+
+        // Pero también es recomendable pasar el token y cualquier otro atributo que queramos pasar al usuario en
+        // una estructura JSON
+        Map<String, Object> body = new HashMap<>();
+        body.put("token", token);
+        body.put("user", (User) authResult.getPrincipal());
+        body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito!", username));
+
+        // Para pasar estos datos a la respuesta obtenemos el writer de la respuesta y escribir, pero
+        // transformando un objeto Map a uno JSON
+        // Para eso se usa ObjectMapper, que permite transformar cualquier objeto Java en un JSON
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+
+        // Indicamos el status de la petición como OK
+        response.setStatus(200);
+
+        // Indicamos el content-type, indicando que retornamos un JSON
+        response.setContentType("application/json");
     }
 }
