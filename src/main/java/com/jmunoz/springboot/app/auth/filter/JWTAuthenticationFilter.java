679174a3eaf7918a6b823d1063121daf3fe761b5
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-09 20:41:39 +0200

    Recibiendo datos del login como JSON raw (Forma 2)

--- src/main/java/com/jmunoz/springboot/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/jmunoz/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.jmunoz.springboot.app.auth.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jmunoz.springboot.app.models.entity.Usuario;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -31,8 +32,6 @@ import java.util.Map;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    // Forma de implementar nuestra propia llave secreta en vez de utilizar la por defecto
-    // Ojo, tiene que ser como mínimo de 256 bits de larga
     public static final SecretKey SECRET_KEY = new SecretKeySpec("algunaLlaveSecretaTienequeser256bitslong".getBytes(), SignatureAlgorithm.HS512.getJcaName());
 
     private AuthenticationManager authenticationManager;
@@ -46,32 +45,43 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
         String username = this.obtainUsername(request);
-        username = username != null ? username : "";
-        username = username.trim();
         String password = this.obtainPassword(request);
-        password = password != null ? password : "";
 
+        // Como estamos enviando datos raw en vez de un form-data estos datos son null, entran por el else
         if (username != null && password != null) {
             logger.info("Username desde request parameter (form-data): " + username);
             logger.info("Password desde request parameter (form-data): " + password);
+        } else {
+            // Tenemos que convertir los datos JSON a un objeto Usuario
+            Usuario user = null;
+            try {
+                user = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+
+                username = user.getUsername();
+                password = user.getPassword();
+
+                logger.info("Username desde request InputStream (raw): " + username);
+                logger.info("Password desde request InputStream (raw): " + password);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
 
         // Se devuelve la autenticación
-        // Esta es una forma cuando se envía el usuario y el password como parámetros del request, por ejemplo
-        // usando Postman.
+        // Implementamos la segunda forma de enviar parámetros a nuestra API Rest, enviando los datos en bruto (raw)
+        // Se usa mucho para integrar frontend con backend
         // Para probar esto en Postman:
         // POST
         // http://localhost:8080/api/login
-        // En Body,
-        // Forma1: Seleccionar form-data
-        //   Las claves-valores que ahí se informan se envían en el request y se recuperan usando request.getParameter()
-        //   Las claves se tienen que llamar username y password
-        //     username     jmunoz
-        //     password     1234
-        // Forma 2: Seleccionar raw y JSON   (Todavía no está hecho)
-        //   Mandar un JSON con el usuario y la contraseña
+        // Forma 2: Seleccionar raw y JSON
+        //   Mandar un JSON con el usuario y la contraseña de esta forma:
+        // {
+        //    "username": "jmunoz",
+        //    "password": 1234
+        // }
         return authenticationManager.authenticate(authToken);
     }
 
@@ -79,20 +89,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
 
-        // Empezamos a crear el JSON Web Token que tenemos que retornar al cliente.
         String username = ((User) authResult.getPrincipal()).getUsername();
 
-        // Obteniendo los roles
-        // El tipo se ha obtenido haciendo Cmd+Click en getAuthorities()
         Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
 
-        // Se va a colocar más información en el token, en concreto la fecha de creación, de expiración y los roles.
-        //
-        // Como no hay un setRoles() ni nada parecido, los roles se añaden como datos extra, llamados Claims.
-        // Lo ideal es que roles se guarden como String. En este caso se está guardando como Object, pero está bien
-        // porque automáticamente se transformará en String.
-        // Pero lo importante es que tenga un formato JSON, es decir, String con estructura JSON.
-        // Si que existe un setClaims para añadir los claims.
         Claims claims = Jwts.claims();
         claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
 
